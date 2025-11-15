ee1ae9e87c86996273fe51976a5d01d33651584b
bian yi min <670614536@qq.com>
2025-11-15 12:27:06 +0800

    开发分页查询功能

--- Project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
+++ Project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nageoffer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
@@ -29,7 +12,6 @@ import java.util.Date;
 
 /**
  * 短链接实体
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 @Data
 @Builder
@@ -103,42 +85,4 @@ public class ShortLinkDO extends BaseDO {
      * 网站标识
      */
     private String favicon;
-
-    /**
-     * 历史PV
-     */
-    private Integer totalPv;
-
-    /**
-     * 历史UV
-     */
-    private Integer totalUv;
-
-    /**
-     * 历史UIP
-     */
-    private Integer totalUip;
-
-    /**
-     * 今日PV
-     */
-    @TableField(exist = false)
-    private Integer todayPv;
-
-    /**
-     * 今日UV
-     */
-    @TableField(exist = false)
-    private Integer todayUv;
-
-    /**
-     * 今日UIP
-     */
-    @TableField(exist = false)
-    private Integer todayUip;
-
-    /**
-     * 删除时间
-     */
-    private Long delTime;
 }
--- Project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ Project/src/main/java/com/nageoffer/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,30 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nageoffer.shortlink.project.dto.resp;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
 
 /**
  * 短链接分页返回参数
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 @Data
 public class ShortLinkPageRespDTO {
@@ -65,21 +46,9 @@ public class ShortLinkPageRespDTO {
     private Integer validDateType;
 
     /**
-     * 启用标识 0：启用 1：未启用
-     */
-    private Integer enableStatus;
-
-    /**
      * 有效期
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
-
-    /**
-     * 创建时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date createTime;
 
     /**
      * 描述
@@ -90,34 +59,4 @@ public class ShortLinkPageRespDTO {
      * 网站标识
      */
     private String favicon;
-
-    /**
-     * 历史PV
-     */
-    private Integer totalPv;
-
-    /**
-     * 今日PV
-     */
-    private Integer todayPv;
-
-    /**
-     * 历史UV
-     */
-    private Integer totalUv;
-
-    /**
-     * 今日UV
-     */
-    private Integer todayUv;
-
-    /**
-     * 历史UIP
-     */
-    private Integer totalUip;
-
-    /**
-     * 今日UIP
-     */
-    private Integer todayUip;
 }
